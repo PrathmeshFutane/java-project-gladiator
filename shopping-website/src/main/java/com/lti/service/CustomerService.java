41f79838e0dbd6aba85afd02851737de9367f4fc
PrathmeshFutane <63443691+PrathmeshFutane@users.noreply.github.com>
2021-04-17 00:39:43 +0530

    this is the changes after login

--- shopping-website/src/main/java/com/lti/service/CustomerService.java
+++ shopping-website/src/main/java/com/lti/service/CustomerService.java
@@ -4,6 +4,7 @@ package com.lti.service;
 import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,6 +29,19 @@ public class CustomerService {
 			Customer updatedCustomer = (Customer) customerRepository.save(customer);
 			//code to send email
 			return updatedCustomer.getCustomerId();
+		}
+	}
+	
+	public Customer login(String email, String password) {
+		try {
+			password = Base64.getEncoder().encodeToString(password.getBytes());
+			int id = customerRepository.fetchIdByEmailAndPassword(email, password);
+			Customer customer = customerRepository.fetch(Customer.class, id);
+			return customer;
+		}
+		catch(EmptyResultDataAccessException e) {
+		//catch(NoResultException e) {
+			throw new CustomerServiceException("Invalid email/password");
 		}
 	}
 }
