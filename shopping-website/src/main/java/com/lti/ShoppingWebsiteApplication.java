e016b2d5eedecce4e6c1695acb4a3c1d692712b3
PrathmeshFutane <63443691+PrathmeshFutane@users.noreply.github.com>
2021-04-16 21:29:35 +0530

    This is the folder structure

---
+++ shopping-website/src/main/java/com/lti/ShoppingWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.lti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingWebsiteApplication.class, args);
+	}
+
+}
---
+++ shopping-website/src/main/java/com/lti/controller/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.lti.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "from shopping project";
+	}
+}
