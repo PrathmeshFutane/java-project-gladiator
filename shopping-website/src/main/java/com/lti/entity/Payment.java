8dd853b395e32f4e1fed5ac06dda383d1f9cadbf
PrathmeshFutane <63443691+PrathmeshFutane@users.noreply.github.com>
2021-04-20 11:57:06 +0530

    created payment entity

--- shopping-website/src/main/java/com/lti/entity/Payment.java
+++ shopping-website/src/main/java/com/lti/entity/Payment.java
@@ -1,5 +1,19 @@
 package com.lti.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "shopping_payment")
 public class Payment {
 
+	@Id
+	@GeneratedValue
+	@Column(name = "payment_id")
+	private int paymentId;
+	
+	 
 }
